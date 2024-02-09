5dca03ed5bec67f25abaa2dea5e7eb9f04a51965
TEJASAI KALLEPALLI <43806122+TejaRaavan@users.noreply.github.com>
2024-02-08 19:20:51 -0600

    custom Exception

---
+++ collections/src/main/java/customException/Main.java
@@ -0,0 +1,19 @@
+package customException;
+
+public class Main {
+    BankingAccount b = new BankingAccount(5000.00);
+public double getCurrentBalance()
+{
+    return b.getBalance();
+}
+    public boolean withDrawlStatus(double amount){
+
+        try{
+            BankingAccount.withDraw(amount);
+            return true;
+        } catch (InvalidWithdrawalException | InsufficientFundsException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
